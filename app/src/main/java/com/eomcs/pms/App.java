367ebe7fa14d4b9ed853db5cbf0c2f129aa9be91
daehooon <22tjssu@naver.com>
2021-01-21 21:59:44 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -1,5 +1,11 @@
 package com.eomcs.pms;
 
+import com.eomcs.pms.handler.BoardHandler;
+import com.eomcs.pms.handler.BoardHandler2;
+import com.eomcs.pms.handler.BoardHandler3;
+import com.eomcs.pms.handler.BoardHandler4;
+import com.eomcs.pms.handler.BoardHandler5;
+import com.eomcs.pms.handler.BoardHandler6;
 import com.eomcs.pms.handler.MemberHandler;
 import com.eomcs.pms.handler.ProjectHandler;
 import com.eomcs.pms.handler.TaskHandler;
@@ -31,6 +37,42 @@ public class App {
             break;
           case "/task/list":
             TaskHandler.list();
+            break;
+          case "/board/add":
+            BoardHandler.add();
+            break;
+          case "/board/list":
+            BoardHandler.list();
+            break;
+          case "/board2/add":
+            BoardHandler2.add();
+            break;
+          case "/board2/list":
+            BoardHandler2.list();
+            break;
+          case "/board3/add":
+            BoardHandler3.add();
+            break;
+          case "/board3/list":
+            BoardHandler3.list();
+            break;
+          case "/board4/add":
+            BoardHandler4.add();
+            break;
+          case "/board4/list":
+            BoardHandler4.list();
+            break;
+          case "/board5/add":
+            BoardHandler5.add();
+            break;
+          case "/board5/list":
+            BoardHandler5.list();
+            break;
+          case "/board6/add":
+            BoardHandler6.add();
+            break;
+          case "/board6/list":
+            BoardHandler6.list();
             break;
           case "quit":
           case "exit":

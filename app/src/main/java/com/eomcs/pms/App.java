c2f167697c3890ad2ab692bdd0701b6dffcabe79
daehooon <22tjssu@naver.com>
2021-01-26 20:51:48 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -22,18 +22,14 @@ public class App {
     MemberHandler memberList = new MemberHandler();
 
     // 각 프로젝트 목록 데이터를 저장할 메모리 준비
-    ProjectHandler projectList = new ProjectHandler();
+    // - 생성자에서 MemberHandler 객체를 주입하라고 강요한다.
+    // - ProjectHandler 객체를 만들려면 반드시 주입해야 한다.
+    ProjectHandler projectList = new ProjectHandler(memberList);
 
-    // ProjectHandler가 의존하는 객체(dependency)를 주입한다.
-    // add() 메서드를 호출할 때 마다 파라미터에 넘기는 대신에
-    // 계속 사용할 수 있도록 인스턴스 필드에 담아 놓는다.
-    projectList.memberList = memberList;
-
-    // 각 작업 목록 데이터를 저장할 메모리 준비
-    TaskHandler taskList = new TaskHandler();
-
-    // TaskHandler가 사용할 의존 객체를 주입한다.
-    taskList.memberList = memberList;
+    // 각 프로젝트 목록 데이터를 저장할 메모리 준비
+    // - 생성자에서 MemberHandler 객체를 주입하라고 강요한다.
+    // - TaskHandler 객체를 만들려면 반드시 주입해야 한다.
+    TaskHandler taskList = new TaskHandler(memberList);
 
     loop:
       while (true) {

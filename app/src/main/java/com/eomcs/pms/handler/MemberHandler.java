316b8274ad25238a0fa502042dc8a7551fa7ac90
daehooon <22tjssu@naver.com>
2021-01-27 23:46:56 +0900

    .

--- app/src/main/java/com/eomcs/pms/handler/MemberHandler.java
+++ app/src/main/java/com/eomcs/pms/handler/MemberHandler.java
@@ -6,7 +6,7 @@ import com.eomcs.util.Prompt;
 public class MemberHandler {
 
   static final int LENGTH = 100;
-  
+
   Member[] members = new Member[LENGTH];  // 레퍼런스 배열 준비  
   int size = 0;
 
@@ -45,6 +45,99 @@ public class MemberHandler {
     }
     return false;
   }
+
+  public void detail() {
+    System.out.println("[회원 상세보기]");
+
+    int no = Prompt.inputInt("번호? ");
+
+    Member member = findByNo(no);
+    if (member == null) {
+      System.out.println("해당 번호의 회원이 없습니다.");
+      return;
+    }
+
+    System.out.printf("이름: %s\n", member.name);
+    System.out.printf("이메일: %s\n", member.email);
+    System.out.printf("전화: %s\n", member.tel);
+    System.out.printf("가입일: %s\n", member.registeredDate);
+
+  }
+
+  // 멤버 번호에 해당하는 인스턴스를 배열에서 찾아 그 인덱스를 리턴한다.
+  int indexOf(int memberNo) {
+    for(int i = 0; i < this.size; i++) {
+      Member member = this.members[i];
+      if(member.no == memberNo) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  // 멤버 번호에 해당하는 인스턴스를 찾아 리턴한다.
+  Member findByNo(int memberNo) {
+    int i = indexOf(memberNo);
+    if (i == -1) 
+      return null;
+    else
+      return this.members[i];
+  }
+
+  public void update() {
+    System.out.println("[회원 변경]");
+
+    int no = Prompt.inputInt("번호? ");
+
+    Member member = findByNo(no);
+    if (member == null) {
+      System.out.println("해당 번호의 회원이 없습니다.");
+      return;
+    }
+
+    String name = Prompt.inputString(String.format("이름(%s)? ", member.name));
+    String email = Prompt.inputString(String.format("이메일(%s)? ", member.email));
+    String tel = Prompt.inputString(String.format("전화(%s)? ", member.tel));
+
+    String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
+
+    if (input.equalsIgnoreCase("Y")) {
+      member.name = name;
+      member.email = email;
+      member.tel = tel;
+      System.out.println("회원을 변경하였습니다.");
+
+    } else {
+      System.out.println("회원 변경을 취소하였습니다.");
+    }
+  }
+
+  public void delete() {
+    System.out.println("[회원 삭제]");
+
+    int no = Prompt.inputInt("번호? ");
+
+    int i = indexOf(no);
+    if (i == -1) {
+      System.out.println("해당 번호의 회원이 없습니다.");
+      return;
+    }
+
+    String input = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ");
+
+    if (input.equalsIgnoreCase("Y")) {
+      for (int x = i + 1; x < this.size; x++) {
+        this.members[x-1] = this.members[x];
+      }
+      members[--this.size] = null; 
+
+      System.out.println("회원을 삭제하였습니다.");
+
+    } else {
+      System.out.println("회원 삭제를 취소하였습니다.");
+    }
+  }
+
 }
 
 
